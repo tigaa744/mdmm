65c1258ee279ea901c6c1f3e65c0eda11c18f65a
tigaa744 <57156833+tigaa744@users.noreply.github.com>
2023-04-09 21:41:20 +0200

    Update homePage.dart
    update home page

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -11,64 +11,81 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   MdmCheck md = new MdmCheck();
+  bool? isMD;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("MDM Check"),
-
       ),
-    body: Column(
-
-      children: [
-
-Text('${MdmCheck.statusMD}'),
-        SizedBox(height: 10,),
-        MaterialButton(
-
-          onPressed: ()
-        {
-          setState(() {
-            md.mdm();
-          });
-
-        },
-
-child: Text('Check MDM'),
-          color: Colors.blue,
-    ),
-   SizedBox(height: 10,),
-
-MaterialButton(
-    child: Text("Remove MDM"),
-    color: Colors.blue,
-    onPressed: (){
-setState(() {
-  md.removeMDMConfigurationProfiles();
-  md.removeMDMEnrollmentInformation();
-
-});
-
-}
-
-),
-      SizedBox(height: 10,),
-        MaterialButton(
-          color: Colors.blue,
-            child: Text("Restart The Device"),
-
-            onPressed: (){
-
-          setState(() {
-md.restartDevice();
-          });
-        }
-
-        )
-
-      ],
-
-    ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              Text('${MdmCheck.statusMD}'),
+              SizedBox(
+                height: 10,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    md.mdm();
+                  });
+                },
+                child: Text('Check MDM'),
+                color: Colors.blue,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text('The result of MDM is $isMD'),
+              MaterialButton(
+                    color: Colors.blue,
+                  child: Text("Check MDM V2 "),
+                  onPressed: () {
+                    setState(() {
+                      isMD = md.isManagedByMDM();
+                    });
+                  }),
+              MaterialButton(
+                child: Text("removeMDMConfigurationProfiles"),
+                color: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    md.removeMDMConfigurationProfiles();
+                  });
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              MaterialButton(
+                  color: Colors.blue,
+                  child: Text("removeMDMEnrollmentInformation"),
+                  onPressed: () {
+                    setState(() {
+                      md.removeMDMEnrollmentInformation();
+                    });
+                  }),
+              SizedBox(
+                height: 10,
+              ),
+              MaterialButton(
+                  color: Colors.blue,
+                  child: Text("Restart The Device"),
+                  onPressed: () {
+                    setState(() {
+                      md.restartDevice();
+                    });
+                  })
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
